0bbdc4adf0c5a8d62dc19b6a6c2348f331f88118
guiviana <guiguiviana70@gmail.com>
2026-03-09 01:16:03 -0300

    Commit 1!

--- teste.cpp
+++ teste.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
     std::cout << "Hello mundo!" << std::endl;
     std::cout << "Hello world!" << std::endl;
-    std::cout << "Hello mundão!" << std::endl;
 
     return 0;
 }
